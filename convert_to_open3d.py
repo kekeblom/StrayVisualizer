ed76b5f8030dc389ed9bc6bcf102b88d5b974a37
Kenneth Blomqvist <kekeblom@gmail.com>
2021-05-19 19:28:23 +0200

    save open3d images in VGA quality, use confidence

--- convert_to_open3d.py
+++ convert_to_open3d.py
@@ -6,10 +6,16 @@ import cv2
 from skvideo import io
 from stray_visualize import DEPTH_WIDTH, DEPTH_HEIGHT, _resize_camera_matrix
 
+FRAME_WIDTH = 1920
+FRAME_HEIGHT = 1440
+OUT_WIDTH = 640
+OUT_HEIGHT = 480
+
 def read_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', type=str)
     parser.add_argument('--out', type=str)
+    parser.add_argument('--confidence', type=int, default=2)
     return parser.parse_args()
 
 def write_frames(flags, rgb_out_dir):
@@ -18,12 +24,19 @@ def write_frames(flags, rgb_out_dir):
     for i, frame in enumerate(video):
         print(f"Writing rgb frame {i:06}" + " " * 10, end='\r')
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-        image = cv2.resize(frame, (DEPTH_WIDTH, DEPTH_HEIGHT))
+        frame = cv2.resize(frame, (OUT_WIDTH, OUT_HEIGHT))
         frame_path = os.path.join(rgb_out_dir, f"{i:06}.jpg")
-        cv2.imwrite(frame_path, image)
+        params = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+        cv2.imwrite(frame_path, frame, params)
+
+def resize_depth(depth):
+    out = cv2.resize(depth, (OUT_WIDTH, OUT_HEIGHT), interpolation=cv2.INTER_NEAREST_EXACT)
+    out[out < 10] = 0
+    return out
 
 def write_depth(flags, depth_out_dir):
     depth_dir_in = os.path.join(flags.dataset, 'depth')
+    confidence_dir = os.path.join(flags.dataset, 'confidence')
     files = sorted(os.listdir(depth_dir_in))
     for filename in files:
         if '.npy' not in filename:
@@ -31,22 +44,25 @@ def write_depth(flags, depth_out_dir):
         print(f"Writing depth frame {filename}", end='\r')
         number, _ = filename.split('.')
         depth = np.load(os.path.join(depth_dir_in, filename))
+        confidence = cv2.imread(os.path.join(confidence_dir, number + '.png'))[:, :, 0]
+        depth[confidence < flags.confidence] = 0
+        depth = resize_depth(depth)
         cv2.imwrite(os.path.join(depth_out_dir, number + '.png'), depth)
 
 def write_intrinsics(flags):
     intrinsics = np.loadtxt(os.path.join(flags.dataset, 'camera_matrix.csv'), delimiter=',')
-    intrinsics_scaled = _resize_camera_matrix(intrinsics, DEPTH_WIDTH / 1920, DEPTH_HEIGHT / 1440)
     data = {}
+    intrinsics_scaled = _resize_camera_matrix(intrinsics, OUT_WIDTH / FRAME_WIDTH, OUT_HEIGHT / FRAME_HEIGHT)
     data['intrinsic_matrix'] = [intrinsics_scaled[0, 0], 0.0, 0.0,
             0.0, intrinsics_scaled[1, 1], 0.0,
             intrinsics_scaled[0, 2], intrinsics_scaled[1, 2], 1.0]
-    data['width'] = DEPTH_WIDTH
-    data['height'] = DEPTH_HEIGHT
+    data['width'] = OUT_WIDTH
+    data['height'] = OUT_HEIGHT
     data['depth_scale'] = 1000.0
     data['fps'] = 60.0
     data['depth_format'] = 'Z16'
     with open(os.path.join(flags.out, 'camera_intrinsics.json'), 'wt') as f:
-        f.write(json.dumps(data))
+        f.write(json.dumps(data, indent=4, sort_keys=True))
 
 def write_config(flags):
     dataset_path = os.path.abspath(os.path.expanduser(flags.out))
@@ -56,11 +72,12 @@ def write_config(flags):
         "path_dataset": dataset_path,
         "path_intrinsic": intrinsics_path,
         "depth_scale": 1000.0,
-        "max_depth": 15.0,
+        "max_depth": 10.0,
+        "min_depth": 0.05,
         "python_multi_threading": False
     }
     with open(os.path.join(dataset_path, 'config.json'), 'w') as f:
-        f.write(json.dumps(config))
+        f.write(json.dumps(config, indent=4, sort_keys=True))
 
 def main():
     flags = read_args()
